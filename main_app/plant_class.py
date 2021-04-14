76428bbc2fd6f0877e58e0d04afdb613c31c0196
jennuwine <jennherrarte93@gmail.com>
2021-04-14 10:29:01 -0700

    hard coded plant kinds

--- main_app/plant_class.py
+++ main_app/plant_class.py
@@ -1,11 +1,13 @@
 class Plant:  # Note that parens are optional if not inheriting from another class
   def __init__(self, name, kind, description):
     self.name = name
-    self.kind = kind
+    self.origin = origin
     self.description = description
 
 
 plants = [
-  Plant('Lolo', 'tabby', 'foul little demon'),
-
+  Plant("Donkey's Tail", "South Mexico", "The Donkey's tail is a succulent plant species that stores water for long periods of time as a fail safe when in its natural habitat water becomes scarce."),
+  Plant("Parlor Palm", "Mexico", "The parlor palm is the most popular of the indoor palm plants variety grown today indoors..."),
+  Plant("Dumb Cane", "West Indies", "The Dumb Cane plant is a strong species that will thrive in any light conditions other than direct sunlight. Its easy to care for style and resilience to neglect make it a wonderful house plant..."),
+  Plant("Swiss Cheese Plant", "South Mexico to Columbia", "The Swiss cheese plant displays the most interesting looking leaves and needs a grower to be prepared to provide some extra space within a home (it grows fairly tall when it matures.")
 ]
