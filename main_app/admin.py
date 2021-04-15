5f7e3693c7df53484dfb8a498e892b351227bba9
jennuwine <jennherrarte93@gmail.com>
2021-04-15 08:40:24 -0700

    added to plant group

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# import your models here
+from .models import Plant
 
-# Register your models here.
+# Register your models here
+admin.site.register(Plant)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-# Create your models here.
+# import your models here
 
 class Plant(models.Model):
     name = models.CharField(max_length=100)
